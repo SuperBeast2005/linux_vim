353cd430ea7e19eeeadf9cf5709e7c3471937d10
SuperBeast2005 <muellerma2023@gmail.com>
2025-03-12 16:43:54 +0100

    database init

--- test/test.py
+++ test/test.py
@@ -4,6 +4,26 @@ import sqlite3
 csv_data  = pd.read_csv('/linux_vim/test.csv')
 df = pd.DataFrame(csv_data)
 
+def create_db():
+    con = sqlite3.connect('/linux_vim/data.db')
+    cur = con.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS male(id, surname, name, email, ip)')
+    cur.execute('CREATE TABLE IF NOT EXISTS female(id, surname, name, email, ip)')
+    con.commit()
+    con.close()
+    
+def insert_male():
+    con = sqlite3.connect('/linux_vim/data.db')
+    cur = con.cursor()
+    data = df[df.gender == 'Male']
+    cur.execute(f'INSERT INTO male  VALUES (?,?,?,?)', data)
+    print(data)
+    con.commit()
+    con.close() 
+
 if __name__ == "__main__":
     print(csv_data)
-    print(df['Gender'])
+    print(df[df.gender  == 'Male'])
+    create_db()
+    insert_male()
+
